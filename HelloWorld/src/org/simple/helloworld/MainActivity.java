2e79908c368682ef1ccfe1a3ddb21e1c73c51f47
Subhashini <psubhas2@ford.com>
2016-08-18 13:57:06 +0530

    Toast from text Field

--- HelloWorld/src/org/simple/helloworld/MainActivity.java
+++ HelloWorld/src/org/simple/helloworld/MainActivity.java
@@ -1,8 +1,14 @@
 package org.simple.helloworld;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +16,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button btn = (Button) findViewById(R.id.button1);
+        
+        btn.setOnClickListener(new OnClickListener() {
+        	
+			@Override
+			public void onClick(View arg0) {
+				Toast toast = Toast.makeText(MainActivity.this, ((EditText) findViewById(R.id.textView1)).getText().toString(), Toast.LENGTH_LONG);
+				toast.show();
+				Log.i("MainActivity", "Inside Toaster");
+			}
+		});
     }
 
 
